5cef2008eda475b3b7277a6c043ca94083d78420
bluemailbox <77300788+bluemailbox@users.noreply.github.com>
2023-05-04 15:28:34 -0700

    Delete 304_lab8_soln_php directory

--- 304_lab8_soln_php/include/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$username = "SA";
-	$password = "YourStrong@Passw0rd";
-	$database = "tempdb";
-	$server = "db";
-	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-?>
--- 304_lab8_soln_php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['authenticatedUser'] = NULL;
-$_SESSION['authenticatedAdmin'] = NULL;
-header('Location: index.php');
-?>
